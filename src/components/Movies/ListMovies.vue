992654a3909fbd7a377278c31a9c3c177573e01c
w3bsite <samfisher217201@gmail.com>
2022-06-23 03:46:59 +0430

    Seprating Pagination Component

--- src/components/Movies/ListMovies.vue
+++ src/components/Movies/ListMovies.vue
@@ -30,36 +30,20 @@
                         </div>
                     </q-card-section>
                 </div>
-
             </q-card>
         </div>
     </div>
+    <!-- Pagination -->
     <div class="ccontainer column md:max-w-80% row mt-30 mb-20">
-        <div class="ma-auto row">
-            <q-btn flat rounded label="Previous Page" color="primary" :disable="page < 2" icon="chevron_left"
-                @click="(page -= 1) && (changePage())" />
-            <q-separator :dark="false" vertical class="mx-2" color="grey-7" />
-            <q-btn flat rounded label="Next Page" color="primary" icon-right="chevron_right"
-                @click="(page += 1) && (changePage())" />
-        </div>
-        <div class="mx-auto mt-2 text-grey-7 text-weight-light">showing results
-            {{ page }}-20
-        </div>
+        <ListPagination></ListPagination>
     </div>
+
 </template>
 <script lang="ts" setup>
 import { useMovie } from '~/store/movie';
-// Page Number
-const page = ref(1)
 // Movie Store
 const store = useMovie()
 // Calling Store Actions To Fetch Contents
 store.getGenres()
-store.getMovies(page.value)
-
-// This Method Is For Fetching Contents Based On Page Changes
-function changePage() {
-    store.getMovies(page.value)
-}
-
+store.getMovies(1)
 </script>
---
+++ src/components/Movies/ListPagination.vue
@@ -0,0 +1,26 @@
+<template>
+    <!-- Pagination -->
+    <div class="ma-auto row">
+        <q-btn flat rounded label="Previous Page" color="primary" :disable="page < 2" icon="chevron_left"
+            @click="(page -= 1) && (changePage())" />
+        <q-separator :dark="false" vertical class="mx-2" color="grey-7" />
+        <q-btn flat rounded label="Next Page" color="primary" icon-right="chevron_right"
+            @click="(page += 1) && (changePage())" />
+    </div>
+    <div class="mx-auto mt-2 text-grey-7 text-weight-light">showing results
+        {{ page }}-20
+    </div>
+
+</template>
+<script lang="ts" setup>
+import { useMovie } from '~/store/movie';
+
+// Page Number
+const page = ref(1)
+// Movie Store
+const store = useMovie()
+// This Method Is For Fetching Contents Based On Page Changes
+function changePage() {
+    store.getMovies(page.value)
+}
+</script>
