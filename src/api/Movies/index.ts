55292e7c3d00e5f4b90944eb16b779d39f0fbd47
w3bsite <samfisher217201@gmail.com>
2022-06-23 02:30:56 +0430

    Short Description For Movies In MovieList && Responsive UI

--- src/api/Movies/index.ts
+++ src/api/Movies/index.ts
@@ -3,10 +3,11 @@ import { AxiosResponse } from "axios";
 import Repository from "@/api/Repository";
 
 export default class Movies extends Repository {
+    // All Movies
     async getMovies(): Promise<AxiosResponse<Movie>> {
         return await this.axios.get("/discover/movie");
     }
-
+    // All Genres
     async getGenres(): Promise<AxiosResponse<Genre>> {
         return await this.axios.get("/genre/movie/list");
     }
--- src/components/Movies/ListMovies.vue
+++ src/components/Movies/ListMovies.vue
@@ -1,28 +1,40 @@
 <template>
-    <div class="row">
+    <div class="ccontainer row md:max-w-80%">
         <!-- Movies List Including Their Genres -->
-        <q-card v-for="(movie, i) in store.GET_MOVIES_GENRES" :key="i" class="pa-4 col-4">
-            <!--  Title -->
-            <q-card-section class="text-weight-medium">
-                {{ movie.title }}
-            </q-card-section>
-            <!--  ReleaseDate -->
-            <q-card-section class=" flex-inline ml-2">
-                <q-icon size="20px" name="calendar_today" class="mr-2"></q-icon>
-                <div>{{ movie.release_date }}</div>
-            </q-card-section>
-            <!--  Genres Array-->
-            <q-card-section>
-                <div v-for="(genre, i) in movie.genres" :key="i" class="  flex-inline ml-2">
-                    <div class="mr-2 text-grey-14"> {{ genre }}</div>
-                    <q-icon size="8px" color="grey-10" class="ma-auto" name="circle"
-                        v-if="movie && movie.genres && movie.genres.length > i + 1" />
+        <div v-for="(movie, i) in store.GET_MOVIES_GENRES" :key="i" class="col-12 col-sm-6 col-md-4 pa-2">
+            <q-card class="md:flex md:flex-row  min-h-335px ">
+                <!-- Movie Thumbnail -->
+
+                <q-img class="col" :src="`https://image.tmdb.org/t/p/w500/${movie.poster_path}`"></q-img>
+
+                <!-- Movie Info -->
+                <div class="col flex">
+                    <!--  Title -->
+                    <q-card-section class="text-weight-medium">
+                        {{ movie.title }}
+                    </q-card-section>
+                    <!-- Short Description -->
+                    <q-card-section class="text-weight-light mt-auto">
+                        {{ movie.overview.substring(0, 70) + '...' }}
+                    </q-card-section>
+                    <!--  ReleaseDate -->
+                    <q-card-section class="flex-inline ml-2 mt-auto">
+                        <q-icon size="20px" name="calendar_today" class="mr-2"></q-icon>
+                        <div>{{ movie.release_date }}</div>
+                    </q-card-section>
+                    <!--  Genres Array-->
+                    <q-card-section>
+                        <div v-for="(genre, i) in movie.genres" :key="i" class="flex-inline ml-2 mt-auto">
+                            <div class="mr-2 text-grey-14"> {{ genre }}</div>
+                            <q-icon size="8px" color="grey-10" class="ma-auto" name="circle"
+                                v-if="movie && movie.genres && movie.genres.length > i + 1" />
+                        </div>
+                    </q-card-section>
                 </div>
-            </q-card-section>
-        </q-card>
-        <!-- <q-card v-for="(genre, i) in store.GET_GENRES" :key="i">
-            {{ genre }}
-        </q-card> -->
+            </q-card>
+        </div>
+
+
     </div>
 </template>
 <script lang="ts" setup>
