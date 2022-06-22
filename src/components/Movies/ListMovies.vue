02f0b44d80db3fef0acdbcd9a24949120d90e88b
w3bsite <samfisher217201@gmail.com>
2022-06-23 04:16:22 +0430

    UI FIX

--- src/components/Movies/ListMovies.vue
+++ src/components/Movies/ListMovies.vue
@@ -1,14 +1,14 @@
 <template>
-    <div class="ccontainer row md:max-w-80%">
+    <div class="ccontainer row md:max-w-90%">
         <!-- Movies List Including Their Genres -->
         <div v-for="(movie, i) in store.GET_MOVIES_GENRES" :key="i" class="col-12 col-sm-6 col-md-4 pa-2">
-            <q-card class="md:flex md:flex-row">
+            <q-card class="md:flex md:flex-row h-100%">
                 <!-- Movie Thumbnail -->
                 <q-img class="col" :src="`https://image.tmdb.org/t/p/w500/${movie.poster_path}`"></q-img>
                 <!-- Movie Info -->
-                <div class="col column">
+                <div class="col md:flex md:flex-col ">
                     <!--  Title -->
-                    <q-card-section class="text-weight-medium ">
+                    <q-card-section class="text-weight-medium md:min-h-25% mb-auto">
                         {{ movie.title }}
                     </q-card-section>
                     <!-- Short Description -->
@@ -16,16 +16,16 @@
                         {{ movie.overview.substring(0, 70) + '...' }}
                     </q-card-section> -->
                     <!--  ReleaseDate -->
-                    <q-card-section class="row mt-auto ml-2">
-                        <q-icon size="20px" name="calendar_today" class="mr-2 my-auto"></q-icon>
-                        <div class="my-auto">{{ movie.release_date }}</div>
+                    <q-card-section class="row pl-18px mb-4 py-0">
+                        <q-icon size="20px" color="grey-9" name="calendar_today" class="mr-2"></q-icon>
+                        <div class="text-grey-8">{{ movie.release_date }}</div>
                     </q-card-section>
                     <!--  Genres Array-->
-                    <q-card-section class="row">
-                        <div v-for="(genre, i) in movie.genres" :key="i" class="row ml-2   ">
-                            <div class="     mr-2  text-grey-14"> {{ genre }}
+                    <q-card-section class="row pt-0">
+                        <div v-for="(genre, i) in movie.genres" :key="i" class="row ml-2px my-auto">
+                            <div class="mr-2px  text-grey-14"> {{ genre }}
                             </div>
-                            <q-icon size="8px" color="grey-9" class="my-auto " name="circle"
+                            <q-icon size="8px" color="grey-9" class=" my-auto " name="circle"
                                 v-if="movie && movie.genres && movie.genres.length > i + 1" />
                         </div>
                     </q-card-section>
